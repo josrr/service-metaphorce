a78489ed58986498379ffcc025cffe0ab1b27508
José M. Á. R. R <josrr@ymail.com>
2024-05-23 15:04:47 -0600

    + New DTO for the response of bulk operations.

---
+++ src/main/java/com/meraphorce/dto/StatusResponse.java
@@ -0,0 +1,19 @@
+package com.meraphorce.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class StatusResponse {
+    private HttpStatus status;
+    private String message;
+}
