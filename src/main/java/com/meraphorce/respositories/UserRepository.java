ac0bc35ea582032fd4125c08d545329020167c3b
José M. Á. R. R <josrr@ymail.com>
2024-05-23 16:29:48 -0600

    + New method findByEmail().

--- src/main/java/com/meraphorce/respositories/UserRepository.java
+++ src/main/java/com/meraphorce/respositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.meraphorce.respositories;
 
 import java.util.List;
+import java.util.Optional;
 import com.meraphorce.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, String>{
     @Query("select u.name from User u")
     List<String> findNames();
+    Optional<User> findByEmail(String email);
 }
