75b3b2e26f1a1832f28515ec78105c6ebfcc9fd8
José M. Á. R. R <josrr@ymail.com>
2024-05-23 15:33:56 -0600

    + New method findNames() to get only the name column.

--- src/main/java/com/meraphorce/respositories/UserRepository.java
+++ src/main/java/com/meraphorce/respositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.meraphorce.respositories;
 
+import java.util.List;
 import com.meraphorce.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
+    @Query("select u.name from User u")
+    List<String> findNames();
 }
