5ef9b099ae5dcc93cfb7dae5f1c629352992e5bd
José M. Á. R. R <josrr@ymail.com>
2024-05-29 10:05:23 -0600

    + createUsersInBulk() now calls the method CreateUsersInBulk() from the service.

--- src/main/java/com/meraphorce/controllers/BulkController.java
+++ src/main/java/com/meraphorce/controllers/BulkController.java
@@ -3,22 +3,15 @@ package com.meraphorce.controllers;
 import com.meraphorce.dto.StatusResponse;
 import com.meraphorce.dto.UserRequest;
 import com.meraphorce.mappers.impl.UserMapper;
-import com.meraphorce.services.UserAlreadyExistsException;
 import com.meraphorce.services.UserService;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Path;
-import jakarta.validation.Validator;
+import jakarta.validation.constraints.Size;
 import java.util.List;
-import java.util.Set;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("/bulk")
@@ -30,45 +23,9 @@ public class BulkController
     @Autowired
     private UserMapper mapper;
 
-    @Autowired
-    private Validator validator;
-
     @PostMapping("/users")
-    public ResponseEntity<List<StatusResponse>> createUsersInBulk(@NotEmpty @RequestBody
-                                                                  List<UserRequest> users) {
-        return ResponseEntity.ok(users.stream().map(user -> {
-                    Set<ConstraintViolation<UserRequest>> violations = validator.validate(user);
-                    if ( violations.isEmpty() ) {
-                        try {
-                            userService.createUser(mapper.requestToEntity(user));
-                            return StatusResponse.builder()
-                                .status(HttpStatus.OK.value())
-                                .message(String.format("New user with name=%s created", user.getName()))
-                                .build();
-                        } catch ( UserAlreadyExistsException ex ) {
-                            return StatusResponse.builder()
-                                .status(HttpStatus.BAD_REQUEST.value())
-                                .message(ex.getMessage())
-                                .build();
-                        }
-                    } else {
-                        StringJoiner joiner = new StringJoiner(", ");
-                        for ( ConstraintViolation<UserRequest> constraintViolation : violations ) {
-                            joiner.add(String.format("%s: %s", getFieldName(constraintViolation),
-                                                     constraintViolation.getMessage()));
-                        }
-                        return StatusResponse.builder()
-                            .status(HttpStatus.BAD_REQUEST.value()).message(joiner.toString())
-                            .build();
-                    }
-                }).collect(Collectors.toList()));
-    }
-
-    private String getFieldName(ConstraintViolation violation) {
-        String field = null;
-        for (Path.Node node : violation.getPropertyPath()) {
-            field = node.getName();
-        }
-        return field;
+    public ResponseEntity<List<StatusResponse>> createUsersInBulk(@NotEmpty @Size(min=1, max=100)
+                                                                  @RequestBody List<UserRequest> users) {
+        return ResponseEntity.ok(userService.createUsersInBulk(users));
     }
 }
