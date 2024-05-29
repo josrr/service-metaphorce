62930a7ad42c0d4accfbc983640198ceb78b1965
José M. Á. R. R <josrr@ymail.com>
2024-05-29 10:03:28 -0600

    + createUser now call repository.existsByEmail() instead     of repository.findByEmail().   + new method createUsersInBulk().

--- src/main/java/com/meraphorce/services/UserService.java
+++ src/main/java/com/meraphorce/services/UserService.java
@@ -1,54 +1,79 @@
 package com.meraphorce.services;
 
+import com.meraphorce.dto.StatusResponse;
+import com.meraphorce.dto.UserRequest;
 import com.meraphorce.models.User;
+import com.meraphorce.processors.UserBulkProcessor;
 import com.meraphorce.respositories.UserRepository;
+import jakarta.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class UserService
 {
+    private final UserRepository repository;
+    private final UserBulkProcessor processor;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository repository, UserBulkProcessor processor) {
+        this.repository = repository;
+        this.processor = processor;
+    }
 
     public User createUser(User user) {
-        Optional<User> userOpt = userRepository.findByEmail(user.getEmail());
-        if ( userOpt.isPresent() )
+        if ( repository.existsByEmail(user.getEmail()) )
             throw new UserAlreadyExistsException(String.format("A user with email=%s already exists.",
                                                                user.getEmail()));
         user.setId(UUID.randomUUID().toString());
-        return userRepository.save(user);
+        return repository.save(user);
+    }
+
+    public List<StatusResponse> createUsersInBulk(List<UserRequest> users) {
+        return users.stream().map(request -> {
+                try {
+                    processor.processUser(request);
+                    return StatusResponse.builder().created(true)
+                        .message(String.format("New user with name=%s created", request.getName()))
+                        .build();
+                } catch ( UserAlreadyExistsException | ConstraintViolationException ex ) {
+                    return StatusResponse.builder().created(false).message(ex.getMessage()).build();
+                } catch ( Exception ex ) {
+                    return StatusResponse.builder().created(false)
+                        .message(String.format("An unexpected error occurred: %s", ex.getMessage()))
+                        .build();
+                }
+            }).collect(Collectors.toList());
     }
 
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return repository.findAll();
     }
 
     public User getUserById(String id) {
-        return userRepository.findById(id)
+        return repository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id=%s not found",
                                                                            id)));
     }
 
     public void deleteUser(String id) {
-        User user = userRepository.findById(id)
+        User user = repository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id=%s not found",
                                                                            id)));
-        userRepository.delete(user);
+        repository.delete(user);
     }
 
     public User updateUser(String id, User request) {
-        if ( ! userRepository.existsById(id) )
+        if ( ! repository.existsById(id) )
             throw new ResourceNotFoundException(String.format("User with id=%s not found", id));
         request.setId(id);
-        return userRepository.save(request);
+        return repository.save(request);
     }
 
     public List<String> getNames() {
-        return userRepository.findNames();
+        return repository.findNames();
     }
 }
