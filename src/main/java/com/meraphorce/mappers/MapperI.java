1432962fec6b0842f7abb0544893fb3a2dc04007
José M. Á. R. R <josrr@ymail.com>
2024-06-01 12:59:51 -0600

    + remove the responseToEntity and entityToRequest methods.

--- src/main/java/com/meraphorce/mappers/MapperI.java
+++ src/main/java/com/meraphorce/mappers/MapperI.java
@@ -2,8 +2,6 @@ package com.meraphorce.mappers;
 
 public interface MapperI<T, RS, RQ>
 {
-    public T responseToEntity(RS response);
     public T requestToEntity(RQ request);
     public RS entityToResponse(T entity);
-    public RQ entityToRequest(T entity);
 }
--- src/main/java/com/meraphorce/mappers/impl/UserMapper.java
+++ src/main/java/com/meraphorce/mappers/impl/UserMapper.java
@@ -1,7 +1,7 @@
 package com.meraphorce.mappers.impl;
 
-import com.meraphorce.dto.UserResponse;
-import com.meraphorce.dto.UserRequest;
+import com.meraphorce.dtos.UserResponse;
+import com.meraphorce.dtos.UserRequest;
 import com.meraphorce.mappers.MapperI;
 import com.meraphorce.models.User;
 import org.springframework.stereotype.Component;
@@ -9,20 +9,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper implements MapperI<User, UserResponse, UserRequest>
 {
-    @Override
-    public User responseToEntity(UserResponse response) {
-        return User.builder()
-            .id(response.getId())
-            .name(response.getName())
-            .email(response.getEmail())
-            .build();
-    }
+    // @Override
+    // public User responseToEntity(UserResponse response) {
+    //     return User.builder()
+    //         .id(response.getId())
+    //         .name(response.getName())
+    //         .email(response.getEmail())
+    //         .build();
+    // }
 
     @Override
     public User requestToEntity(UserRequest request) {
         return User.builder()
             .name(request.getName())
             .email(request.getEmail())
+            .password(request.getPassword())
+            .roles(request.getRoles())
             .build();
     }
 
@@ -32,14 +34,15 @@ public class UserMapper implements MapperI<User, UserResponse, UserRequest>
             .id(entity.getId())
             .name(entity.getName())
             .email(entity.getEmail())
+            // .roles(entity.getRoles())
             .build();
     }
 
-    @Override
-    public UserRequest entityToRequest(User entity) {
-        return UserRequest.builder()
-            .name(entity.getName())
-            .email(entity.getEmail())
-            .build();
-    }
+    // @Override
+    // public UserRequest entityToRequest(User entity) {
+    //     return UserRequest.builder()
+    //         .name(entity.getName())
+    //         .email(entity.getEmail())
+    //         .build();
+    // }
 }
