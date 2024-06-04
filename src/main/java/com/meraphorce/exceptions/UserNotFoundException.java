a108ea4f3ff45b01bf4f4e313e250d9349d0e523
José M. Á. R. R <josrr@ymail.com>
2024-06-04 08:18:02 -0600

    + Moved all exceptions to the package com.meraphorce.exceptions

---
+++ src/main/java/com/meraphorce/exceptions/UserNotFoundException.java
@@ -0,0 +1,20 @@
+package com.meraphorce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception thrown when a user is not found in the database.
+ * This results in a HTTP 404 Not Found response.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException
+{
+    /**
+     * Constructs a new UserNotFoundException with the specified detail message.
+     * @param message the detail message.
+     */
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
